946f8c87c20587846a1aa941b962d13cca1543e8
kareneliza <karen_weeks@fitnyc.edu>
2022-12-22 19:20:37 -0500

    choob

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const app = express();
 const port = 8080;
 
 
-app.use(express.static(__dirname + '/public/'));
-app.use('/css', express.static(__dirname + 'public/css/'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/data', express.static(__dirname + 'public/data/'))
-app.use('/img', express.static(__dirname + 'public/img'))
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/homepage.html')
 })
